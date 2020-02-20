44a8e6e38087d3b1382392dabc96cb7c3a5b7cb7
Jiwon Han <42082640+jiwon-seattle@users.noreply.github.com>
2020-02-19 23:27:35 -0800

    Update scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,38 @@
 $(document).ready(function(){
     $("#stress_survey").submit(function(event){
-      event.preventDefault();
-      //$("#work-responses").show();
-      //var userResponses = []
+      event.preventDefault()
+        
       sum1 = 0
       sum2 = 0
       sum3 = 0
 
       $("input:checkbox[name=negativeAffects]:checked").each(function(){
-        var affects = parseInt($(this).val());
+        var affects = parseInt($(this).val())
         sum1 = sum1 + affects
         
-        });
+        })
 
       $("input:checkbox[name=symptoms]:checked").each(function(){
-        var affects = parseInt($(this).val());
+        var affects = parseInt($(this).val())
         sum2 = sum2 + affects
          
-        });
-
+        })
+        
       $("input:checkbox[name=coping]:checked").each(function(){
-        var affects = parseInt($(this).val());
+        var affects = parseInt($(this).val())
         sum3 = sum3 + affects
              
-        });
+        })
 
-                                                         
-
-    sum = sum1 + sum2 + sum3
+      sum = sum1 + sum2 + sum3
     
-    if (sum >= 5) {
-        $("#extreme").show();
-    } else if (sum >= 3 || sum <5) {
-        $("#medium").show();
-    } else {
-        $("#low").show();
-    }
-
+      if (sum >= 5) {
+        $("#extreme").show()
+      } else if (sum >= 3 || sum <5) {
+        $("#medium").show()
+      } else {
+        $("#low").show()
+      }
     
-});
-    
-});
+    })   
+})
